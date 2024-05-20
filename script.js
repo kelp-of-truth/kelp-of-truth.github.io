498cffb5f9570eb21eb2546f19106deff57c433a
kelp of truth <102844552+kelp-of-truth@users.noreply.github.com>
2024-05-20 18:44:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const searchParams = new URLSearchParams(window.location.search)
-for (const [key, val] of searchParams) {
-    if(key==="name"){
-        document.querySelector("#title").innerHTML=val;
-        document.querySelector("#player").src=`./src/${val}`;
+window.addEventListener("load",()=>{
+    const main=document.querySelector(".main");
+    const header=document.querySelector(".header");
+    header.classList.add("visible")
+    main.hidden=false;
+    document.querySelector(".footer").style.display="inline-block";
+    setTimeout(() => {
+        main.classList.add("visible");
+    }, 1);
+})
+// window.addEventListener("scroll",()=>{
+//     if(window.scrollY+document.querySelector(".apple-music iframe").clientHeight>document.querySelector(".apple-music").getBoundingClientRect().y){
+//         setTimeout(() => {
+//             document.querySelector(".apple-music iframe").classList.add("visible");
+//         }, 1);
+//     }
+// })
+// let copied_timer=0;
+function contentCopied(n){
+    if(n>0){
+        setTimeout(() => {
+            document.querySelector(".content_copy img").src="./img/content_copied.png";
+            document.querySelector(".content_copy img").alt="content_copied";
+            n-=1;
+            contentCopied(n);
+        }, 1);
+    }else{
+        document.querySelector(".content_copy img").src="./img/content_copy.png";
+        document.querySelector(".content_copy img").alt="content_copy";
+        return true;
     }
 }
+function ad(n){
+    if(n===0){
+        alert(":thinking:")
+    }
+}
+setInterval(() => {
+    if(document.getElementById("aswift_7").clientHeight===0){
+        for(let idx of document.querySelectorAll(".ad")){
+            idx.remove();
+        }
+    }
+}, 10);
