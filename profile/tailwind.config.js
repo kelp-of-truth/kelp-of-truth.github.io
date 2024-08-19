742fbb592b9f7b5cae41364723b13ed50c5cd63d
kelp of truth <102844552+kelp-of-truth@users.noreply.github.com>
2024-08-19 09:12:01 +0900

    Add files via upload

---
+++ profile/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme:{
+        extend:{
+            boxShadow: {
+                hv:"0px 0px 10px #00000080"
+            },
+            fontFamily: {
+                inconsolata:['"Inconsolata"', 'monospace'],
+                play:['"Play"', 'sans-serif']
+            }
+        }
+    }
+}
